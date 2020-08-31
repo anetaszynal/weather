99259918af9386e952b0aa970418c8d67133f8dc
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-31 20:14:04 +0200

    feat(Footer): add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Footer = () => (
+  <footer>
+    <p>&copy; Aneta Szynal</p>
+  </footer>
+);
