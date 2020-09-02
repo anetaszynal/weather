f714be5c0f52925a34846aa130290676f95c694b
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-02 22:44:29 +0200

    feat(main): add weather display

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { useDate } from "./useDate";
 import { formatDate } from "../../lib/utils";
+import { useWeather } from "./useWeather";
+import rain from "../../assets/rain.svg";
 
 export const Main = () => {
   const [city, setCity] = useState("");
   const date = useDate();
+  const weatherData = useWeather();
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -13,16 +16,26 @@ export const Main = () => {
   return (
     <main>
       <h2>{`Witaj! Dziś jest ${formatDate(date)}`}</h2>
-      <form onSubmit={onFormSubmit}>
-        <label>
-          <p>Wpisz nazwę miasta, dla którego chcesz sprawdzić pogodę :</p>
-          <input
-            name="city"
-            placeholder="Wpisz nazwę miasta"
-            onChange={({ target }) => setCity(target.value)}
-          ></input>
-        </label>
-      </form>
+      {city === "" ? (
+        <form onSubmit={onFormSubmit}>
+          <label>
+            <p>Wpisz nazwę miasta, dla którego chcesz sprawdzić pogodę :</p>
+            <input
+              name="city"
+              placeholder="Wpisz nazwę miasta"
+              onChange={({ target }) => setCity(target.value)}
+            ></input>
+          </label>
+        </form>
+      ) : (
+        <div>
+          <p>{`Pogoda dla ${weatherData.data.name}`} </p>
+          <img src={rain} alt=""></img>
+          <p>
+            {`Dziś przygotuj się na ${weatherData.data.weather[0].description}`}{" "}
+          </p>
+        </div>
+      )}
     </main>
   );
 };
