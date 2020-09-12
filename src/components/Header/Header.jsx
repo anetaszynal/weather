58d09784079c0ac38d4667f3f9f1369b67158d1f
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-12 23:19:51 +0200

    feat(header): remove header

--- src/components/Header/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import {Title} from "./styled";
-import {Content} from "../../lib/Style/styled";
-
-export const Header = () => (
-    <Content>
-        <Title>Pogoda na dziś !</Title>
-    </Content>
-);
