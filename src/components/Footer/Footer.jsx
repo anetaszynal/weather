a13e76442d0926ca0c509ad60569f5dc4528ac46
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-12 16:55:13 +0200

    feat(footer): add style

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import {Content, Paragraph} from "../../lib/Style/styled";
 
 export const Footer = () => (
-  <footer>
-    <p>&copy; Aneta Szynal</p>
-  </footer>
+    <Content as="footer">
+        <Paragraph>Strona wykonana przez Aneta Szynal</Paragraph>
+        <Paragraph>Dane pogodowe pobrane zostały ze strony: https://www.accuweather.com</Paragraph>
+    </Content>
 );
