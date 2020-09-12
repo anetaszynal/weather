dca323b561e8bf0477899c8da0f6a48fe04492b6
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-12 23:31:57 +0200

    refactor(): format

--- src/lib/Icon.jsx
+++ src/lib/Icon.jsx
@@ -1,162 +1,162 @@
 export const Icon = [
-  {
-    id: 1,
-    icon: require("../assets/1.png"),
-  },
-  {
-    id: 2,
-    icon: require("../assets/2-4.png"),
-  },
-  {
-    id: 3,
-    icon: require("../assets/2-4.png"),
-  },
-  {
-    id: 4,
-    icon: require("../assets/2-4.png"),
-  },
-  {
-    id: 5,
-    icon: require("../assets/5.png"),
-  },
-  {
-    id: 6,
-    icon: require("../assets/6.png"),
-  },
-  {
-    id: 7,
-    icon: require("../assets/7.png"),
-  },
-  {
-    id: 8,
-    icon: require("../assets/8.png"),
-  },
-  {
-    id: 11,
-    icon: require("../assets/11.png"),
-  },
-  {
-    id: 12,
-    icon: require("../assets/12.png"),
-  },
-  {
-    id: 13,
-    icon: require("../assets/13-14.png"),
-  },
-  {
-    id: 14,
-    icon: require("../assets/13-14.png"),
-  },
-  {
-    id: 15,
-    icon: require("../assets/15.png"),
-  },
-  {
-    id: 16,
-    icon: require("../assets/16-17.png"),
-  },
-  {
-    id: 17,
-    icon: require("../assets/16-17.png"),
-  },
-  {
-    id: 18,
-    icon: require("../assets/18.png"),
-  },
-  {
-    id: 19,
-    icon: require("../assets/19.png"),
-  },
-  {
-    id: 20,
-    icon: require("../assets/20-21,23.png"),
-  },
-  {
-    id: 21,
-    icon: require("../assets/20-21,23.png"),
-  },
-  {
-    id: 22,
-    icon: require("../assets/22.png"),
-  },
-  {
-    id: 23,
-    icon: require("../assets/20-21,23.png"),
-  },
-  {
-    id: 24,
-    icon: require("../assets/24-25.png"),
-  },
-  {
-    id: 25,
-    icon: require("../assets/24-25.png"),
-  },
-  {
-    id: 26,
-    icon: require("../assets/26,29.png"),
-  },
-  {
-    id: 29,
-    icon: require("../assets/26,29.png"),
-  },
-  {
-    id: 30,
-    icon: require("../assets/30.png"),
-  },
-  {
-    id: 31,
-    icon: require("../assets/31.png"),
-  },
-  {
-    id: 32,
-    icon: require("../assets/32.png"),
-  },
-  {
-    id: 33,
-    icon: require("../assets/33.png"),
-  },
-  {
-    id: 34,
-    icon: require("../assets/34-35.png"),
-  },
-  {
-    id: 35,
-    icon: require("../assets/34-35.png"),
-  },
-  {
-    id: 36,
-    icon: require("../assets/36,38.png"),
-  },
-  {
-    id: 37,
-    icon: require("../assets/37.png"),
-  },
-  {
-    id: 38,
-    icon: require("../assets/36,38.png"),
-  },
-  {
-    id: 39,
-    icon: require("../assets/39.png"),
-  },
-  {
-    id: 40,
-    icon: require("../assets/40.png"),
-  },
-  {
-    id: 41,
-    icon: require("../assets/41-42.png"),
-  },
-  {
-    id: 42,
-    icon: require("../assets/41-42.png"),
-  },
-  {
-    id: 43,
-    icon: require("../assets/43-44.png"),
-  },
-  {
-    id: 44,
-    icon: require("../assets/43-44.png"),
-  },
+    {
+        id: 1,
+        icon: require("../assets/1.png"),
+    },
+    {
+        id: 2,
+        icon: require("../assets/2-4.png"),
+    },
+    {
+        id: 3,
+        icon: require("../assets/2-4.png"),
+    },
+    {
+        id: 4,
+        icon: require("../assets/2-4.png"),
+    },
+    {
+        id: 5,
+        icon: require("../assets/5.png"),
+    },
+    {
+        id: 6,
+        icon: require("../assets/6.png"),
+    },
+    {
+        id: 7,
+        icon: require("../assets/7.png"),
+    },
+    {
+        id: 8,
+        icon: require("../assets/8.png"),
+    },
+    {
+        id: 11,
+        icon: require("../assets/11.png"),
+    },
+    {
+        id: 12,
+        icon: require("../assets/12.png"),
+    },
+    {
+        id: 13,
+        icon: require("../assets/13-14.png"),
+    },
+    {
+        id: 14,
+        icon: require("../assets/13-14.png"),
+    },
+    {
+        id: 15,
+        icon: require("../assets/15.png"),
+    },
+    {
+        id: 16,
+        icon: require("../assets/16-17.png"),
+    },
+    {
+        id: 17,
+        icon: require("../assets/16-17.png"),
+    },
+    {
+        id: 18,
+        icon: require("../assets/18.png"),
+    },
+    {
+        id: 19,
+        icon: require("../assets/19.png"),
+    },
+    {
+        id: 20,
+        icon: require("../assets/20-21,23.png"),
+    },
+    {
+        id: 21,
+        icon: require("../assets/20-21,23.png"),
+    },
+    {
+        id: 22,
+        icon: require("../assets/22.png"),
+    },
+    {
+        id: 23,
+        icon: require("../assets/20-21,23.png"),
+    },
+    {
+        id: 24,
+        icon: require("../assets/24-25.png"),
+    },
+    {
+        id: 25,
+        icon: require("../assets/24-25.png"),
+    },
+    {
+        id: 26,
+        icon: require("../assets/26,29.png"),
+    },
+    {
+        id: 29,
+        icon: require("../assets/26,29.png"),
+    },
+    {
+        id: 30,
+        icon: require("../assets/30.png"),
+    },
+    {
+        id: 31,
+        icon: require("../assets/31.png"),
+    },
+    {
+        id: 32,
+        icon: require("../assets/32.png"),
+    },
+    {
+        id: 33,
+        icon: require("../assets/33.png"),
+    },
+    {
+        id: 34,
+        icon: require("../assets/34-35.png"),
+    },
+    {
+        id: 35,
+        icon: require("../assets/34-35.png"),
+    },
+    {
+        id: 36,
+        icon: require("../assets/36,38.png"),
+    },
+    {
+        id: 37,
+        icon: require("../assets/37.png"),
+    },
+    {
+        id: 38,
+        icon: require("../assets/36,38.png"),
+    },
+    {
+        id: 39,
+        icon: require("../assets/39.png"),
+    },
+    {
+        id: 40,
+        icon: require("../assets/40.png"),
+    },
+    {
+        id: 41,
+        icon: require("../assets/41-42.png"),
+    },
+    {
+        id: 42,
+        icon: require("../assets/41-42.png"),
+    },
+    {
+        id: 43,
+        icon: require("../assets/43-44.png"),
+    },
+    {
+        id: 44,
+        icon: require("../assets/43-44.png"),
+    },
 ];
