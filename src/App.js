f0e1d31cb3a7e408b90f69c1ff8bdc677b22de17
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-12 23:24:54 +0200

    feat(App): add style to loading and failure process

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 import React from "react";
 import "./App.css";
-import { Header } from "./components/Header";
-import { Form } from "./components/Form";
-import { Footer } from "./components/Footer";
-import { Weather } from "./components/Weather";
-import { useWeather } from "./useWeather";
+import {Form} from "./components/Form";
+import {Footer} from "./components/Footer";
+import {Weather} from "./components/Weather";
+import {useWeather} from "./useWeather";
+import {Loading, Failure} from "./styled";
 
 function App() {
-  const { weather, city, getCityName, fetchData } = useWeather();
+    const {weather, city, getCityName, fetchData} = useWeather();
 
-  const onFormCityButtonClick = (event) => {
-    event.preventDefault();
-    fetchData();
-  };
+    const onFormCityButtonClick = (event) => {
+        event.preventDefault();
+        fetchData();
+    };
 
-  return (
-    <div>
-      <Header />
-      {weather.state === "loading" ? (
-        <div>
-          <p>Proszę czekać, trwa pobieranie danych ...</p>
-        </div>
-      ) : (
-        <Form
-          getCityName={getCityName}
-          onFormCityButtonClick={onFormCityButtonClick}
-        />
-      )}
-      {weather.state === "error" && (
-        <div>
-          <p>
-            Nieudało się pobrać danych. Sprawdź czy poprawnie wpisałeś nazwę
-            miejscowości
-          </p>
-        </div>
-      )}
-      {weather.state === "succes" && <Weather city={city} weather={weather} />}
-      <Footer />
-    </div>
-  );
+    return (
+        <>
+            <Form
+                getCityName={getCityName}
+                onFormCityButtonClick={onFormCityButtonClick}
+            />
+            {weather.state === "loading" && (
+                <Loading>Proszę czekać, trwa pobieranie danych ...</Loading>
+            )}
+            {weather.state === "error" && (
+                <Failure>
+                    Niestety :( Nie udało się pobrać danych. Sprawdź czy poprawnie wpisałeś nazwę
+                    miejscowości.
+                </Failure>
+            )}
+            {weather.state === "succes" && <Weather city={city} weather={weather}/>}
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
