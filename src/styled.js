d55065ea5327e001d0c0d6b2ce214ddc4e08b2a3
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-12 23:28:49 +0200

    feat(app): add style to loading and failure

---
+++ src/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Loading = styled.p`
+   margin: 20px;
+   font-size: 20px;
+   font-weight: bold;
+   color: #192a56;
+   line-height: 1.5;
+   text-align: center;
+`;
+
+export const Failure = styled.p`
+   margin: 20px;
+   font-size: 20px;
+   font-weight: bold;
+   color: #ff0000cf;
+   line-height: 1.5;
+   text-align: center;
+`;
