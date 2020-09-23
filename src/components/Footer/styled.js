91b9cf779ff2900606ceb40cc1285d0646802948
Aneta Szynal <anetaszynal28@gmail.com>
2020-09-23 23:10:59 +0200

    style(Footer): change padding

--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Paragraph = styled.p`
   margin: 0;
-  padding: 10px 0 10px 20px;
+  padding: 25px 0 30px 20px;
   font-size: 15px;
   color: #192a56;
 `;
